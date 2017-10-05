96345c7a2562b3fd872b800b24cf129030e97df2
Johannes Eiglsperger <git@remodding.de>
2017-10-06 00:14:42 +0200

    Fix static file

--- edupy/settings.py
+++ edupy/settings.py
@@ -121,7 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'edupy/static'),
-]
+
 STATIC_URL = '/static/'
