5bdca4c74f9e688262985dae2cc42a33b42b7547
Johannes Eiglsperger <git@remodding.de>
2017-12-12 21:15:21 +0100

    Correct URL syntax for Braindump

--- categories/urls.py
+++ categories/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/edit/$', CategoryUpdate.as_view(), name='category-update'),
     url(r'^(?P<pk>[0-9]+)/delete/$', CategoryDelete.as_view(), name='category-delete'),
     url(r'^braindump/$', braindump_index, name='braindump-index'),
-    url(r'^braindump/(?P<category_pk>[0-9]+)/$', braindump_session, name='braindump-session'),
-    url(r'^braindump/(?P<category_pk>[0-9]+)/card/(?P<card_pk>[0-9]+)/ok$', braindump_ok, name='braindump-ok'),
-    url(r'^braindump/(?P<category_pk>[0-9]+)/card/(?P<card_pk>[0-9]+)/nok$', braindump_nok, name='braindump-nok'),
+    url(r'^(?P<category_pk>[0-9]+)/braindump/$', braindump_session, name='braindump-session'),
+    url(r'^(?P<category_pk>[0-9]+)/braindump/card/(?P<card_pk>[0-9]+)/ok$', braindump_ok, name='braindump-ok'),
+    url(r'^(?P<category_pk>[0-9]+)/braindump/card/(?P<card_pk>[0-9]+)/nok$', braindump_nok, name='braindump-nok'),
 ]
