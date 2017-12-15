2c2ca0fe2666903f4eda91bb17d750bd2e4d5dbb
Johannes Eiglsperger <git@remodding.de>
2017-12-15 14:20:58 +0100

    Settings structure

---
+++ edupy/settings/development.py
@@ -0,0 +1,29 @@
+"""Django development settings
+"""
+
+from .base import *
+
+print('Loading development settings')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'q)4!4@-rbkbrb&_+$5$@-y8o)wglp6qgip#@)03n8xmh5m9xs7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+    }
+}
