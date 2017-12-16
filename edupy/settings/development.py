db7eff3af82018d5362b9c59845f865d8b653d2f
Johannes Eiglsperger <git@remodding.de>
2017-12-16 21:14:06 +0100

    Fix path for development database

--- edupy/settings/development.py
+++ edupy/settings/development.py
@@ -24,6 +24,6 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
