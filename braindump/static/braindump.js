864b4517e0180d8cfe4ab3b838f2125ade7b5b23
Johannes Eiglsperger <git@remodding.de>
2018-05-11 20:25:36 +0200

    Move Braindump's JavaScript to dedicated JS block

--- braindump/static/braindump.js
+++ braindump/static/braindump.js
@@ -1,9 +1,11 @@
-$("#braindump #show-hint").click(function() {
-    $("#braindump #hint").show();
-    $(this).addClass("disabled");
-});
+$(document).ready(function(){
+    $("#braindump #show-hint").click(function() {
+        $("#braindump #hint").show();
+        $(this).addClass("disabled");
+    });
 
-$("#braindump #show-answer").click(function() {
-    $("#braindump #answer").show();
-    $(this).addClass("disabled");
+    $("#braindump #show-answer").click(function() {
+        $("#braindump #answer").show();
+        $(this).addClass("disabled");
+    });
 });
