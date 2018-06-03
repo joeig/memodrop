cec7ab33642cc99740bd965dc1bf7c883355dc0f
Johannes Eiglsperger <git@remodding.de>
2018-06-03 22:24:03 +0200

    Bump version: 1.0.4 → 1.0.5

--- memodrop/__about__.py
+++ memodrop/__about__.py
@@ -7,7 +7,7 @@ __all__ = [
 ]
 
 __name__ = 'memodrop'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __description__ = 'Rapid learning process for people with tight schedules.'
 __url__ = 'https://github.com/joeig/memodrop'
 __author__ = 'Johannes Eiglsperger'
